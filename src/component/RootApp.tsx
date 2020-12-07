2b68944838901a82430032bc34e8c1413e5d8e3e
Geoffrey Berard <geoffrey.berard@gmail.com>
2020-12-07 10:26:55 +0100

    Reveal - Bootstrap

--- src/component/RootApp.tsx
+++ src/component/RootApp.tsx
@@ -1,4 +1,28 @@
 import * as React from "react";
 import { FC } from "react";
+import Reveal from "reveal.js";
+import RevealNotes from "reveal.js/plugin/notes/notes.esm";
+import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm";
 
-export const RootApp: FC = () => <div>Hello World !</div>;
+export const initReveal = () =>
+  Reveal.initialize({
+    hash: true,
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+  });
+
+export const RootApp: FC = () => {
+  React.useEffect(initReveal, []);
+
+  return (
+    <div className="slides">
+      <section>Horizontal Slide</section>
+      <section>
+        <section>Vertical Slide 1</section>
+        <section>Vertical Slide 2</section>
+        <section>Vertical Slide 3</section>
+      </section>
+    </div>
+  );
+};
