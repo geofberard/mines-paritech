72db3c2bb8066d37d63b243bcf68466e2d32eb62
gbe <geoffrey.berard@lesfurets.com>
2023-11-15 14:28:52 +0100

    Docker - Adding pdf

--- src/docker/section/MainTitle.tsx
+++ src/docker/section/MainTitle.tsx
@@ -10,6 +10,6 @@ export const MainTitle = () => (
         <Title>Develop faster - Run anywhere</Title>
         <SubTitle>Geoffrey Berard / <a href="https://www.linkedin.com/in/geoffrey-berard-a21b9347">@geofberard</a></SubTitle>
         <SubTitle>geoffrey.berard@gmail.com</SubTitle>
-        <SubTitle><a href="http://bit.ly/3SHpELP">http://bit.ly/3SHpELP</a></SubTitle>
+        <SubTitle><a href="http://bit.ly/3SHpELP">http://bit.ly/3SHpELP - <a href="./docker.pdf" target="_blank"> pdf</a></a></SubTitle>
     </Slide>
 );
